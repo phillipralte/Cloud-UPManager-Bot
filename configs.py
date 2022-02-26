60bfe0be7d53db20ee4c1aa4f61e721d196f76ee
phillipralte <100476382+phillipralte@users.noreply.github.com>
2022-02-26 21:17:07 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("1340222")
-	API_HASH = os.environ.get("334133d7097f902e7c3776dbe5ab9ea8")
-	BOT_TOKEN = os.environ.get("5227373896:AAF-QdagR3i_X6-rClQ4S7TYMAyJRSr_blY")
+	API_ID = int(os.environ.get('1340222')
+	API_HASH = os.environ.get('334133d7097f902e7c3776dbe5ab9ea8')
+	BOT_TOKEN = os.environ.get('5227373896:AAF-QdagR3i_X6-rClQ4S7TYMAyJRSr_blY')
 	GOFILE_API = os.environ.get("GOFILE_API")
 	STREAMTAPE_API_PASS = os.environ.get("6RV1XvxmZMc9D4Z")
 	STREAMTAPE_API_USERNAME = os.environ.get("6234b79c86891789d4ec")
