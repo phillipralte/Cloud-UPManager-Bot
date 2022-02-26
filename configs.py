d5aa4942b4d54f62aa71ea13545e60416f304b86
phillipralte <100476382+phillipralte@users.noreply.github.com>
2022-02-26 23:19:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = os.environ.get("API_ID"))
+	API_ID = os.environ.get("API_ID")
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	GOFILE_API = os.environ.get("GOFILE_API")
 	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
 	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
 	SESSION_NAME = os.environ.get("SESSION_NAME", "CloudManagerBot")
-	BOT_OWNER = os.environ.get("BOT_OWNER"))
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL"))
+	BOT_OWNER = os.environ.get("BOT_OWNER")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
 	DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
 	HELP_TEXT = """
 Send me any Media & Choose Upload Server,
