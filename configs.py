57bbcaee1b13f895463cfd10d8d4f078c96a6a7d
phillipralte <100476382+phillipralte@users.noreply.github.com>
2022-02-26 22:57:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,14 +4,14 @@ import os
 
 
 class Config(object):
-	API_ID = int("1340222")
+	API_ID = os.environ.get("1340222")
 	API_HASH = os.environ.get("334133d7097f902e7c3776dbe5ab9ea8")
 	BOT_TOKEN = os.environ.get("5227373896:AAF-QdagR3i_X6-rClQ4S7TYMAyJRSr_blY")
 	GOFILE_API = os.environ.get("GOFILE_API")
 	STREAMTAPE_API_PASS = os.environ.get("6RV1XvxmZMc9D4Z")
 	STREAMTAPE_API_USERNAME = os.environ.get("6234b79c86891789d4ec")
 	SESSION_NAME = os.environ.get("SESSION_NAME", "streamLogosBot")
-	BOT_OWNER = int(os.environ.get("2054421248"))
+	BOT_OWNER = os.environ.get("2054421248")
 	
 	DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
 	HELP_TEXT = """
